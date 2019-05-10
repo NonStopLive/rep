eded6dbf6df799c483c0f1dd568026feb9b1c3e9
JuanPablo <horman0056@gmail.com>
2019-05-10 11:58:37 +0200

    Skrypta dodauem ni

--- projekt_1/pacjent.php
+++ projekt_1/pacjent.php
@@ -25,7 +25,21 @@
 				<th>Jan Nowak</th><th>kardiolog</th>
 			</tr>
 		</table>
-		<h3>LISTA PACJENTÓW</h3>
+        <h3>LISTA PACJENTÓW</h3>
+        <?php 
+			$polacz = mysqli_connect('localhost','root','','poradnia');
+			$zap = "SELECT id, imie, nazwisko, choroba FROM `pacjenci`";
+			$wynik = mysqli_query($polacz,$zap);
+			while ($l=mysqli_fetch_row($wynik)) {
+				echo $l[0].' '.$l[1].' '.$l[2].' '.$l[3].'<br>';
+			}
+			mysqli_close($polacz);
+		?>
+        <br><br>
+		<form method="post" action="pacjent.php">
+			<label>Podaj id: <br>
+				<input type="number" name="id"></label>
+				<input type="submit" value="Pokaż szczegóły">
 		</form>
 </div>
 	<div class="right">
