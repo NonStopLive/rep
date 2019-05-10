9eca174ef0cae8300ab87777c0e764a9f19e613d
JuanPablo <horman0056@gmail.com>
2019-05-10 11:43:25 +0200

    Ty rób do poradni ja zrobię do pacjenta

---
+++ projekt_1/pacjent.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="poradnia.css">
+	<title>Poradnia</title>
+</head>
+<body>
+	<header>
+		<h1>PORADNIA SPECJALISTYCZNA</h1>
+	</header>
+	<div class="left">
+		<h3>LEKARZE SPECJALIŚCI</h3>
+		<table>
+			<tr>
+				<th colspan="2">Poniedziałek</th>
+			</tr>
+			<tr>
+				<th>Anna Kowalska</th><th>otolaryngolog</th>
+			</tr>
+			<tr>
+				<th colspan="2">Wtorek</th>
+			</tr>
+			<tr>
+				<th>Jan Nowak</th><th>kardiolog</th>
+			</tr>
+		</table>
+		<h3>LISTA PACJENTÓW</h3>
+		</form>
+</div>
+	<div class="right">
+		<h2>KARTA PACJENTA</h2>
+</div>
+	<footer>
+		<p>utworzone przez: Na pewno nie Michał </p>
+		<a href="kwerendy.txt">Kwerendy do pobrania</a>
+	</footer>
+</body>
+</html>
