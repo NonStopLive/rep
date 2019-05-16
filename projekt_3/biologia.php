df27c01a95736b0ff30a7effc4d31ff1ada069e9
MaDMen <fabianson100@o2.pl>
2019-05-16 11:06:15 +0200

    Misiu Masz tutaj HTML i CSS. Zrób Skrypta <3

---
+++ projekt_3/biologia.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="styl.css">
+<title>Szkoła Podstawowa</title>
+
+</head>
+<body>
+
+<div class="baner">
+
+	<h1>Oceny uczniów: biologia</h1>
+
+</div>
+<div class="left">
+
+<h2>Uczeń: </h2>
+
+</div>
+<div class="right">
+
+<h3>Nazwiska i numery PESEL uczniów: </h3>
+
+</div>
+<div class="stopka">
+
+<h3>Szkoła Podstawowa</h3>
+<p>Stronę opracował: Fabian i Hubert</p>
+
+</div>
+
+</body>
+</html>
