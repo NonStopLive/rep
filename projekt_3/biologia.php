733232ccf64633f9e9e416d535247799da1d9386
JuanPablo <horman0056@gmail.com>
2019-05-16 11:17:42 +0200

    Zrobiłem skrypta i wgl koniec już

--- projekt_3/biologia.php
+++ projekt_3/biologia.php
@@ -17,18 +17,47 @@
 </div>
 <div class="left">
 
-<h2>Uczeń: </h2>
-
+<h2>Uczeń: <?php 
+				$polacz = mysqli_connect('localhost','root','','szkola');
+				$zap = "SELECT `uczen`.`imie`, `uczen`.`nazwisko` FROM `uczen` WHERE `uczen`.`id`=1";
+				$wynik = mysqli_query($polacz,$zap);
+				while ($l=mysqli_fetch_row($wynik)) {
+					echo $l[0].' '.$l[1];
+				}
+				mysqli_close($polacz);
+			 ?>
+		</h2>
+		<p>Najwyższa ocena z biologii: 
+			<?php 			 
+				$polacz = mysqli_connect('localhost','root','','szkola');
+				$zap = "SELECT MAX(`ocena`.`ocena`) FROM `ocena` INNER JOIN `uczen` On `uczen`.`id`= `ocena`.`uczen_id` Where `uczen`.`id` = 1 AND `ocena`.`przedmiot_id`= 4";
+				$wynik = mysqli_query($polacz,$zap);
+				while ($l=mysqli_fetch_row($wynik)) {
+					echo $l[0];
+				}
+				mysqli_close($polacz);	 
+			?>
+		</p>
 </div>
 <div class="right">
 
 <h3>Nazwiska i numery PESEL uczniów: </h3>
-
+		<ul>
+			<?php 				 
+				$polacz = mysqli_connect('localhost','root','','szkola');
+				$zap = "SELECT `uczen`.`nazwisko`, `uczen`.`PESEL` FROM `uczen`";
+				$wynik = mysqli_query($polacz,$zap);
+				while ($l=mysqli_fetch_row($wynik)) {
+					echo '<li>'.$l[0].' '.$l[1].'</li>';
+				}
+				mysqli_close($polacz);			
+			?>
+		</ul>
 </div>
 <div class="stopka">
 
 <h3>Szkoła Podstawowa</h3>
-<p>Stronę opracował: Fabian i Hubert</p>
+<p>Stronę opracował: Puchaty i Prosiaty</p>
 
 </div>
 
