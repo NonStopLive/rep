eb80c388207e6cbe606708b369c1b21d18285061
Umberto <horman0056@gmail.com>
2019-05-27 22:27:07 +0200

    Jeszcze trochu

--- SKLYP/head.php
+++ SKLYP/head.php
@@ -11,6 +11,38 @@
 session_start();
     $polacz = mysqli_connect('localhost','root','','sklyp');
     mysqli_query($polacz,"set names utf8");
+
+    $conditons_totalPrice = '';
+    
+    $getTotal = 0;
+    
+    $idsProduct = '';
+    $qtysProduct = '';
+
+    $separator = ',';
+    foreach($_SESSION['koszyk'] as $key => $total_price) {
+        
+        
+        $sql = "SELECT cena FROM produkt WHERE id = ".$total_price['id'];
+        $result = mysqli_query($polacz,$sql);
+        $getPrice = mysqli_fetch_array($result);
+        
+        if($total_price['ilosc'] > 1) { 
+            $ilosc = $total_price['ilosc'];
+        } else { 
+            $ilosc = 1;
+        }
+         $getTotal += $getPrice['cena']*$ilosc;
+
+         if($key < count($_SESSION['koszyk'])) {
+         $idsProduct .= $total_price['id'].$separator;
+            $qtysProduct .= $total_price['ilosc'].$separator;
+        } else {
+            $idsProduct .= $total_price['id'];
+            $qtysProduct .= $total_price['ilosc'];
+        }
+         
+    }
     ?>
 
 </head>
--- SKLYP/zamowienie.php
+++ SKLYP/zamowienie.php
@@ -4,6 +4,7 @@
 
 <?php
 include('head.php');
+$checked='checked';
 ?>
 
 <body>
@@ -20,11 +21,16 @@ include('head.php');
 <img class="koszyknapis" src="koszyknapis.png"> <a href="sklyp-koszyk.php"> <img class="koszyk" src="strzałka.png"> </a>
 <br>
 
-<img class="koszyknapis" src="zrealizowane.png">
-
 </div>
 <div class="karton">
 
+<input type="checkbox" name="Kurier Tomek" <?php if ($checked == '1') $getTotal+2;?>/><?php echo 'Kurier Tomek'.'</br>'?> 
+
+<input type="checkbox" name="Paczkomat" <?php if ($checked == '1') $getTotal+1;?> /><?php echo 'Paczkomat'.'</br>'?> 
+<input type="checkbox" name="Punkt" <?php if ($checked== '1') $getTotal+1;?> /><?php echo 'Punkt'.'</br>'?> 
+<?php
+echo $getTotal;
+?>
 
 </div>
 
